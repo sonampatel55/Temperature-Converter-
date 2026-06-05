ad3b57f19ede55821936dac89ce8977d64de9e9f
sonampatel55 <sonampatel2955@gmail.com>
2026-06-05 21:15:48 +0530

    tem converter file

---
+++ task2.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class Demo
+{
+public static void main(String ar[])
+{
+	double temp;
+	String unit;
+	double newtem;
+	
+Scanner sc=new Scanner(System.in);
+
+
+System.out.println("Enter the tempreture");
+temp=sc.nextDouble();
+
+
+System.out.println("Convert into Fahrenheit or Celsius(C or F)");
+unit=sc.next().toUpperCase();
+
+
+if(unit.equals("C"))
+{
+	newtem=(temp-32)*5/9;
+}
+else
+{
+	newtem=(temp*5/9)+32;
+}
+ System.out.printf("%.1f°%s",newtem,unit);
+ 
+}
+}
